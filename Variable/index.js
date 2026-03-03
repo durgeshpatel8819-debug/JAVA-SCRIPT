1abda8d6baf696fe50fdd0b338418c43e1a1d118
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-03 20:52:57 +0530

    update

---
+++ Variable/index.js
@@ -0,0 +1,23 @@
+// variable
+
+let name="Durgesh";
+let age=20;
+age=30;
+console.log(name);
+console.log(age);
+
+// const
+
+
+const account =12345;
+console.log(account);
+// account=23;
+console.log(account);
+
+// var
+
+var a =10;
+console.log(a);
+a=20;
+console.log(a);
+
