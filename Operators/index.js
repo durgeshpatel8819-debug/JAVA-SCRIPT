e5ae4cbe7d7f2199aa0b0c5a9cde5d6c0d8597a1
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-16 23:46:35 +0530

    update

---
+++ Operators/index.js
@@ -0,0 +1,55 @@
+// Arithmetic
+
+console.log(2+3);
+console.log(2+5);
+console.log(2+8);
+
+// reminder
+
+console.log(5**3);
+
+// Assignment
+
+let x=20;
+let y=10;
+x=x-y;
+console.log(x);
+
+// comparison
+
+let a=20;
+let b="20";
+console.log(a>b);
+console.log(a==b);
+console.log(a===b);//phele type check hoga uske baad comparsion check hoga
+
+// string to number
+let c="120";
+let d=Number(c);
+console.log(d);
+
+// number to string
+
+let e=50;
+let f=String(e);
+console.log(typeof f);
+
+// boolean convet
+
+console.log(Boolean(10));
+console.log(Boolean(0));
+
+// computer science problem
+
+let g="0.1";
+let h="0.2";
+let i=(g+h);
+console.log(i==0.3);
+
+// 1: null is looselt undefined only
+console.log(null==undefined);
+console.log(null===undefined);
+
+// >,<,>=,<=(null --> number,undefined -->nan)
+
+console.log(null>=0);
