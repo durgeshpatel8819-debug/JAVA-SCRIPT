f7a3f4540b86d63029a2cb9f76f4a37c7b38464a
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-17 00:08:56 +0530

    update

---
+++ Loop/index.js
@@ -0,0 +1,23 @@
+// loop
+
+// for loop
+
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+// while loop
+
+let i=0
+while(i<10){
+    console.log(i)
+    i++;
+}
+
+// do while loop
+
+do{
+    console.log(i);
+    i++;
+
+}while(i<10);
