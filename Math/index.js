3f4af9e6ba9c9333ca0f09c95ab039658cb63926
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-17 23:45:30 +0530

    update

---
+++ Math/index.js
@@ -0,0 +1,13 @@
+console.log(Math.abs(-4));
+console.log(Math.PI);
+console.log(Math.LN10);
+console.log(Math.LN2);
+console.log(Math.ceil(2.3));
+console.log(Math.floor(2.3));
+console.log(Math.max(2.3,400,10,1));
+console.log(Math.floor(Math.random()*10)+1);//0 to 1 value is generates 0 is include
+console.log(Math.floor(Math.random()*6)+1);
+
+// OTP generates 4 digit :1000-9999
+
+console.log(Math.floor(Math.random()*(9999-1000+1)+1000));
