e58752d0af35031fa0946ab02befe193e55cb2c9
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-17 22:46:47 +0530

    update

---
+++ Number/index.js
@@ -0,0 +1,14 @@
+let a=10;
+let b=345.6821;
+console.log(typeof b.toFixed(1));
+
+console.log(b.toPrecision(6));
+console.log(b.toString());
+
+let c= new Number(20)  //number as a object
+console.log(typeof c);
+// non permitive refrences bases compare
+// permitive copy by value
+let x=10;
+let y=10;
+console.log(x==y);
