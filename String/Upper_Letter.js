4627489927b053313f5a8d7e29fe300567f08de2
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-18 19:52:33 +0530

    update

---
+++ String/Upper_Letter.js
@@ -0,0 +1,5 @@
+const str=`durgesh patel`;
+console.log(str.toUpperCase());
+console.log(str.indexOf('rg'));//first time present
+console.log(str.lastIndexOf('e'));//last time present
+console.log(str.includes('e'));//present or not
