acc4de8106e7d9f8e0b12794879494ade805ddc6
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-18 19:39:38 +0530

    update

---
+++ String/index.js
@@ -0,0 +1,9 @@
+const str1="durgesh";
+const str2='Adarsh';
+const day=18;
+const str3=`Vikas ${day}`;//backtick is use in multiple line
+console.log(str1,str2,str3);
+
+const str=`hello durgesh`;
+console.log(str.length);
+console.log(str[1]);
