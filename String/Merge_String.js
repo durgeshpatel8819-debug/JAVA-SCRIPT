72ab2d33f4cd1d7929e5a2c8239ff1f99f72ab60
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-18 22:43:21 +0530

    update

---
+++ String/Merge_String.js
@@ -0,0 +1,8 @@
+const str1="Durgesh";
+const str2="Patel";
+const c=str1+" "+str2;
+console.log(c);
+
+//replace
+const str = `I am a durgesh patel`;
+console.log(str.replace(`durgesh`,`vikas`));
---
+++ String/Slice.js
@@ -0,0 +1,5 @@
+const str="Durgesh patel";
+console.log(str.slice(2,5));
+console.log(str.slice(2));
+console.log(str.slice(-4));
+console.log(str.slice(-5,-2));
