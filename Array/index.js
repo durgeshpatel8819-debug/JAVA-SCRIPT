e039d13032eefefdf724d3dcc91981c329a9015f
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-19 23:08:49 +0530

    update

---
+++ Array/index.js
@@ -0,0 +1,31 @@
+// let marks1=100;
+// let marks2=10;
+// let marks3=140;
+// let marks4=150;
+
+let marks =[100,50,70,20,80];
+console.log(marks);
+console.log(marks.length);
+
+let arr =[100,30,"durgesh",true];
+console.log(arr);
+console.log(arr[1]);
+arr[1]=90;
+console.log(arr);
+arr.push(10);//push operation at end
+console.log(arr);
+
+//pop operation at end
+arr.pop();
+console.log(arr);
+
+// adding at starting and delete
+
+arr.unshift(10);
+console.log(arr);
+
+// delete
+arr.shift();
+console.log(arr);
+
+
