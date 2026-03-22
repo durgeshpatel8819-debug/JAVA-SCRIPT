53ad5bd4e3c355a2259ef43cbf8883cc945de0fc
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-22 20:13:08 +0530

    update

---
+++ Array/Sorting.js
@@ -0,0 +1,8 @@
+const names= ["Alice","Durgesh","Vikas","Adarsh"];
+console.log(names.sort());//comapre on the ASCII value
+console.log(names.reverse());
+
+
+const a= [101,90,98,32,91];
+console.log(a.sort());
+console.log(a.sort((a,b)=> a-b)); //comapre as a number as ascending order
