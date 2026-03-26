2cb24bc1fdcdfcbce0d1d8bfe762300e937ba5e9
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-26 22:40:38 +0530

    update

---
+++ Function/index1.js
@@ -0,0 +1,21 @@
+// function greeting(){
+//     console.log("Hello Durgesh");
+// }
+// greeting();
+
+// function addNumber(num1,num2,num3=0,num4=0){
+//     const sum=num1+num2+num3+num4;
+//     console.log(sum);
+
+// }
+
+//rest operator
+function addNumber(...num){
+    let sum =0;
+    for(let n of num){
+        sum=sum+n;
+    }
+    console.log(sum)}
+addNumber(3,4);
+addNumber(3,4,5);
+addNumber(3,4,5,6);
