6e6e6ad27a416940254027797c610de2006bf1e0
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-26 23:02:06 +0530

    update

---
+++ Function/index3.js
@@ -0,0 +1,13 @@
+//arrow function
+// const addNumber=(num1,num2)=>{
+//    return num1+num2;
+    
+// }
+// console.log(addNumber(3,4));
+
+//arrow function
+// const addNumber=(num1,num2)=>num1+num2;
+// console.log(addNumber(3,4));
+
+const squareNumber=(num)=>num*num;
+console.log(squareNumber(6));
---
+++ Function/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+const addNumber=(num1,num2)=>num1+num2;
+// console.log(addNumber(3,4));
