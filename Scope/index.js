3f54938edea384db847323b76c89709a93bfa346
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-03-29 15:14:14 +0530

    update

---
+++ Scope/index.js
@@ -0,0 +1,14 @@
+//global scope all can be access
+//functional scope to the function
+//block scope to block
+let a=10;
+const b=20;
+var e=90;
+console.log(a);
+function greet(){
+    let c=30;
+    console.log(c);
+   
+}
+
+greet();
