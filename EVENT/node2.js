981e7d7f4f3a6de6f4c8498eb25f99b116afe03f
durgeshpatel8819-debug <durgeshpatel8819@gmail.com>
2026-04-09 01:49:44 +0530

    update

---
+++ EVENT/node2.js
@@ -0,0 +1,24 @@
+// const child1=document.getElementById("child1");
+// child1.addEventListener('click',()=>{
+//     child1.textContent="I am clicked";
+
+// })
+
+// const parent=document.getElementById("parent");
+// console.log(parent.children);
+
+// for(let child of parent.children){
+//     child.addEventListener('click',()=>{
+//       child.textContent ="i am clicked";  
+//     })
+// }
+
+
+const parent=document.getElementById("parent");
+console.log(parent.children);
+
+for(let child of parent.children){
+    child.addEventListener('click',()=>{
+      child.textContent ="i am clicked";  
+    })
+}
